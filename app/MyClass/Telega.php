57bc55ed1883e9e97c8945f71ae9f24c1fd301b7
Qvei <serg198590@gmail.com>
2022-06-19 15:46:36 +0300

    changes

--- app/MyClass/Telega.php
+++ app/MyClass/Telega.php
@@ -7,14 +7,14 @@ class Telega {
 
 
     //protected $http;
-    const $url = 'https://api.telegram.org/bot';
+    //const $url = 'https://api.telegram.org/bot';
 
     // public function __construct($http){
     //     $this->http = $http;
     // }
 
     public function sendMessage($chat_id, $message){
-            \Illuminate\Support\Facades\Http::post($url . env('BOT_TOKEN') . '/sendMessage',[
+            \Illuminate\Support\Facades\Http::post('https://api.telegram.org/bot' . env('BOT_TOKEN') . '/sendMessage',[
                 'chat_id' => $chat_id,
                 'text' => $message,
                 'parse_mod' => 'html',
