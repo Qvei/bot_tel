8c233e2e4b68f65b0d76869966a91f146fb8111a
Qvei <serg198590@gmail.com>
2022-06-19 15:25:01 +0300

    changes

--- app/MyClass/Telega.php
+++ app/MyClass/Telega.php
@@ -1,14 +1,33 @@
 <?php
 namespace App\MyClass;
 
+use Illuminate\Support\Facades\Http;
+
 class Telega {
 
+
+    protected $http;
+    const $url = 'https://api.telegram.org/bot';
+
+    public function __construct($http){
+        $this->http = $http;
+    }
+
     public function sendMessage($chat_id, $message){
-        \Illuminate\Support\Facades\Http::post('https://api.telegram.org/bot'.env('BOT_TOKEN').'/sendMessage',[
+            return $this->http::post($url . env('BOT_TOKEN') . '/sendMessage',[
                 'chat_id' => $chat_id,
                 'text' => $message,
                 'parse_mod' => 'html',
             ]);
+    }
+
+    public function sendDocument($chat_id, $file){
+            return $this->http::attach('document' . Storage::get('/public/123.png'), 'document.png')
+                ->post($url . env('BOT_TOKEN') . '/sendDocument',[
+                    'chat_id' => $chat_id,
+                    // 'text' => $message,
+                    // 'parse_mod' => 'html',
+                ]);
     }
 
 }
