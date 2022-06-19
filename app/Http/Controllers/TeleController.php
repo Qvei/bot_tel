a9abd01de1afd2e3421ea173fda192515ba64c2b
Qvei <serg198590@gmail.com>
2022-06-19 19:02:30 +0300

    test

--- app/Http/Controllers/TeleController.php
+++ app/Http/Controllers/TeleController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\MyClass\Telega;
 use Illuminate\Http\Request;
 
 class TeleController extends Controller
@@ -15,8 +15,7 @@ class TeleController extends Controller
         if(isset($data['message']))
             $data = $data['message'];
 
-        $message = mb_strtolower(($data['text'] ? $data['text']
-            : $data['data']) , 'utf-8' );
+        $message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']) , 'utf-8' );
         $method = 'sendMessage';
         switch ($message){
             case '/start':
@@ -33,21 +32,13 @@ class TeleController extends Controller
         return $this->sendTelegram($method,$send_data);
     }
 
-    private function sendTelegram($method,$data,$headers=[]){
+    private function sendTelegram($method,$data){
 
-        $handle = curl_init('https://api.telegram.org/bot' . env('BOT_TOKEN') . '/' .$method);
-        curl_setopt($handle, CURLOPT_POST, true);
-        curl_setopt($handle, CURLOPT_HEADER, 0);
-        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($handle, CURLOPT_POSTFIELDS, json_encode($data));
-        curl_setopt($handle, CURLOPT_HTTPHEADER,
-            array_merge( array("Content-Type: application/json"),
-                $headers ) );
-        curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
-        curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+    	$telega = new Telega();
+    	$telega->sendMessage($data['chat_id'], $data['text']);
 
-        $result = curl_exec($handle);
-        curl_close($handle);
+        
+        
         return ( json_decode($result,1) ? json_decode($result,1) :
             $result);
     }
