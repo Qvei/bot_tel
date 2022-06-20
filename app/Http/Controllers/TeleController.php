051fe54b780e9a244b2b718f2b0db1556d9c940b
Qvei <serg198590@gmail.com>
2022-06-20 12:52:30 +0300

    add weather test

--- app/Http/Controllers/TeleController.php
+++ app/Http/Controllers/TeleController.php
@@ -9,6 +9,7 @@ class TeleController extends Controller
     public function get_data_from_tg(){
         $content = file_get_contents("php://input");
         $data = json_decode($content, true);
+        $knopki = false;
 
         if(isset($data['callback_query']))
             $data = $data['callback_query'];
@@ -21,8 +22,8 @@ class TeleController extends Controller
                 	'inline_keyboard' => [
                 		[
                 			[
-                				'text' => 'button1',
-                				'callback_data' => '1'
+                				'text' => 'забруднення',
+                				'callback_data' => 'повітря'
                 			],
                 		],
                 		[
@@ -38,13 +39,55 @@ class TeleController extends Controller
                 $send_data = [
                     'text'=>'Hi'
                 ];
-                
+                $knopki = true;
                 break;
+            case 'повітря':
+            	$curl = curl_init();
+                    curl_setopt_array($curl, array(
+                    CURLOPT_URL => $youadress = "http://api.openweathermap.org/data/2.5/air_pollution?lat=49.435345&lon=24.234243&lang=uk&appid=11a6e571a195073ce358513e1f5e46e8",
+                    CURLOPT_RETURNTRANSFER => true,
+                                    CURLOPT_FOLLOWLOCATION => true,
+                                    CURLOPT_ENCODING => "",
+                                    CURLOPT_MAXREDIRS => 10,
+                                    CURLOPT_TIMEOUT => 30,
+                                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                                    CURLOPT_CUSTOMREQUEST => "GET",
+                ));
+                $resp = curl_exec($curl);
+                $err = curl_error($curl);
+                curl_close($curl);
+                $resp = json_decode($resp, true);
+                foreach ($resp['list'] as $val) {
+                        $ono = $val['main']['aqi'];
+                        $tim = $val['dt'];
+                        $wno = $val['components']['co'];
+                        $lno = $val['components']['no'];
+                        $qno = $val['components']['no2'];
+                        $xno = $val['components']['o3'];
+                        $cno = $val['components']['so2'];
+                        $jno = $val['components']['pm2_5'];
+                        $vno = $val['components']['pm10'];
+                        $ano = $val['components']['nh3'];      
+                }
+        if($ono == 1){
+                        $ans = "<b>Рівень забрудненості повітря в цьому районі вважається задовільним</b> ".iconv('UCS-4LE', 'UTF-8', pack('V', 0x1F603))."\n\nЗабруднення повітря становить невеликий ризик або взагалі не становить його.\n\n<b>Оновлено:</b> ".Carbon::parse($tim)->diffForHumans()."\n\n<b>Якість повітря (AQI)</b> ".$ono."\n<b>Монооксид вуглецю (CO)</b> ".$wno."\n<b>Нітроген діоксид (NO2)</b> ".$qno."\n<b>Концентрація озону (O3)</b> ".$xno."\n<b>Діокси́д сі́рки (SO2)</b> ".$cno."\n<b>Частки пилу PM2_5</b> ".$jno."\n<b>Частки пилу PM10</b> ".$vno."\n<b>Окси́д азо́ту(II)</b> ".$lno;
+                    }elseif($ono == 2){
+                        $ans = "<b>Рівень забрудненості повітря в цьому районі важається прийнятним</b> ".iconv('UCS-4LE', 'UTF-8', pack('V', 0x1F60F))."\n\nОднак для деяких забруднювачів може існувати помірне занепокоєння щодо здоров'я дуже невеликої кількості людей, які надзвичайно чутливі до забруднення повітря.\n\n<b>Оновлено:</b> ".Carbon::parse($tim)->diffForHumans()."\n\n<b>Якість повітря (AQI)</b> ".$ono."\n<b>Монооксид вуглецю (CO)</b> ".$wno."\n<b>Нітроген діоксид (NO2)</b> ".$qno."\n<b>Концентрація озону (O3)</b> ".$xno."\n<b>Діокси́д сі́рки (SO2)</b> ".$cno."\n<b>Частки пилу PM2_5</b> ".$jno."\n<b>Частки пилу PM10</b> ".$vno."\n<b>Окси́д азо́ту(II)</b> ".$lno;
+                    }elseif($ono == 3){
+                        $ans =  "<b>Рівень забрудненості повітря в цьому районі вважається не чистим</b> ".iconv('UCS-4LE', 'UTF-8', pack('V', 0x1F612))."\n\nЧлени чутливих груп можуть відчувати наслідки для здоров'я. Населення, швидше за все, не постраждає.\n\n<b>Оновлено:</b> ".Carbon::parse($tim)->diffForHumans()."\n\n<b>Якість повітря (AQI)</b> ".$ono."\n<b>Монооксид вуглецю (CO)</b> ".$wno."\n<b>Нітроген діоксид (NO2)</b> ".$qno."\n<b>Концентрація озону (O3)</b> ".$xno."\n<b>Діокси́д сі́рки (SO2)</b> ".$cno."\n<b>Частки пилу PM2_5</b> ".$jno."\n<b>Частки пилу PM10</b> ".$vno."\n<b>Окси́д азо́ту(II)</b> ".$lno;
+                    }elseif($ono == 4){
+                        $ans = "<b>Рівень забрудненості повітря в цьому районі вважається дуже поганим</b> ".iconv('UCS-4LE', 'UTF-8', pack('V', 0x1F614))."\n\nКожен може почати відчувати наслідки для здоров’я; члени чутливих груп можуть відчувати більш серйозні наслідки для здоров'я.\n\n<b>Оновлено:</b> ".Carbon::parse($tim)->diffForHumans()."\n\n<b>Якість повітря (AQI)</b> ".$ono."\n<b>Монооксид вуглецю (CO)</b> ".$wno."\n<b>Нітроген діоксид (NO2)</b> ".$qno."\n<b>Концентрація озону (O3)</b> ".$xno."\n<b>Діокси́д сі́рки (SO2)</b> ".$cno."\n<b>Частки пилу PM2_5</b> ".$jno."\n<b>Частки пилу PM10</b> ".$vno."\n<b>Окси́д азо́ту(II)</b> ".$lno;
+                    }elseif($ono == 5){
+                        $ans = "<b>Рівень забрудненості повітря в цьому районі надзвичайно поганий</b> ".iconv('UCS-4LE', 'UTF-8', pack('V', 0x1F62D))."\n\nПопередження про стан здоров’я в надзвичайних ситуаціях. Все населення, швидше за все, постраждає.\n\n<b>Оновлено:</b> ".Carbon::parse($tim)->diffForHumans()."\n\n<b>Якість повітря (AQI)</b> ".$ono."\n<b>Монооксид вуглецю (CO)</b> ".$wno."\n<b>Нітроген діоксид (NO2)</b> ".$qno."\n<b>Концентрація озону (O3)</b> ".$xno."\n<b>Діокси́д сі́рки (SO2)</b> ".$cno."\n<b>Частки пилу PM2_5</b> ".$jno."\n<b>Частки пилу PM10</b> ".$vno."\n<b>Окси́д азо́ту(II)</b> ".$lno;
+                    }
+                    $send_data = [
+	                    'text'=> $ans,
+	                ];
+            	break;
             default:
                 $send_data = [
                     'text'=>'Try another text'
                 ];
-                //$buttons = [];
         }
         $send_data['chat_id']=$data['chat']['id'];
         return $this->sendTelegram($method,$send_data,$buttons);
@@ -53,7 +96,7 @@ class TeleController extends Controller
     private function sendTelegram($method,$data,$buttons){
 
     	$telega = new Telega();
-    	$telega->sendMessage($data['chat_id'], $data['text']);
+    	//$telega->sendMessage($data['chat_id'], $data['text']);
     	return $telega->sendButtons($data['chat_id'], $data['text'], json_encode($buttons));
 
     }
