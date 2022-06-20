6fb0af02cd8d1d2981de6a7b184c8ec12690b2d7
Qvei <serg198590@gmail.com>
2022-06-20 13:36:21 +0300

    add weather test

--- app/MyClass/Telega.php
+++ app/MyClass/Telega.php
@@ -1,41 +1,40 @@
 <?php
 namespace App\MyClass;
 
-//use Illuminate\Support\Facades\Http;
 
 class Telega {
 
 
     //protected $http;
-    //const $url = 'https://api.telegram.org/bot';
+    const $url = 'https://api.telegram.org/bot';
 
     // public function __construct($http){
     //     $this->http = $http;
     // }
 
     public function sendMessage($chat_id, $message){
-            return \Illuminate\Support\Facades\Http::post('https://api.telegram.org/bot' . env('BOT_TOKEN') . '/sendMessage',[
+            return \Illuminate\Support\Facades\Http::post($url . '' . env('BOT_TOKEN') . '/sendMessage',[
                 'chat_id' => $chat_id,
                 'text' => $message,
-                'parse_mod' => 'html',
+                'parse_mode' => 'HTML',
             ]);
     }
 
     public function sendButtons($chat_id, $message, $buttons){
-            return \Illuminate\Support\Facades\Http::post('https://api.telegram.org/bot' . env('BOT_TOKEN') . '/sendMessage',[
+            return \Illuminate\Support\Facades\Http::post($url . '' . env('BOT_TOKEN') . '/sendMessage',[
                     'chat_id' => $chat_id,
                     'text' => $message,
-                    'parse_mod' => 'html',
+                    'parse_mode' => 'HTML',
                     'reply_markup' => $buttons,
                 ]);
     }
 
     public function sendDocument($chat_id, $file){
             return \Illuminate\Support\Facades\Http::attach('document' . Storage::get('/public/123.png'), 'document.png')
-                ->post($url . env('BOT_TOKEN') . '/sendDocument',[
+                ->post($url . '' . env('BOT_TOKEN') . '/sendDocument',[
                     'chat_id' => $chat_id,
                     // 'text' => $message,
-                    // 'parse_mod' => 'html',
+                    // 'parse_mode' => 'html',
                 ]);
     }
 
