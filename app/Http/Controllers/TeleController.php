b2aaa75d2424776173687d2a832e67d55e116eff
Qvei <serg198590@gmail.com>
2022-06-22 01:25:24 +0300

    test

--- app/Http/Controllers/TeleController.php
+++ app/Http/Controllers/TeleController.php
@@ -67,8 +67,8 @@ class TeleController extends Controller
 	            	$wear = new NewClass($url);
 	                $ans = $wear->addaAnsver();
 	            	$url2 = "https://api.openweathermap.org/data/2.5/onecall?lat=".$latitude."&lon=".$longitude."&exclude=daily&lang=ua&appid=".env('WEATHER_KEY');
-					$result = json_decode(file_get_contents($url), true);
-					$weather = "Температура повітря - ".round(floatval($result['current']['temp']) - 273.15); 
+					$re = json_decode(file_get_contents($url2), true);
+					$weather = "Температура повітря - ".round(floatval($re['current']['temp']) - 273.15); 
 					$ans .= "\n\n" . $weather;
                     $send_data = [
 	                    'text'=> $ans,
