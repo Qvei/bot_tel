922d8d4a862c27d263b5d82c0532513b2290c070
Qvei <serg198590@gmail.com>
2022-06-23 13:13:38 +0300

    test

--- app/Http/Controllers/TeleController.php
+++ app/Http/Controllers/TeleController.php
@@ -63,7 +63,7 @@ class TeleController extends Controller
 
             $send_data['chat_id'] = $chat_id;
 
-        return $this->sendTelegram($method,$send_data,$buttons);
+        return $this->sendTelegram($send_data,$buttons);
     }
 
 
