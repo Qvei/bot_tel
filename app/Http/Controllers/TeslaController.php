2a43029ea5bfce0195f6ecf47113ca26ce0d600d
Qvei <serg198590@gmail.com>
2022-06-25 19:14:25 +0300

    try

--- app/Http/Controllers/TeslaController.php
+++ app/Http/Controllers/TeslaController.php
@@ -14,13 +14,13 @@ class TeslaController extends Controller
     
     public function get_data(Request $request)
     {
-    	$data = 'test';
-        //$data = Http::post('https://owner-api.teslamotors.com/'.$request->oauth.'/'.$request->token);
+    	//$data = 'test';
+        $data = Http::post('https://owner-api.teslamotors.com/'.$request->oauth.'/'.$request->token);
         
-        echo $data;
+        //echo $data;
        // echo $data->access_token;
-       //$data = json_encode($data, true);
-        
+       $data = json_encode($data, true);
+        echo $data;
 
        
        }
