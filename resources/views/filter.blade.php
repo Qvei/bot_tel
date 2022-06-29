cbba239b152b38bafea2ce213babfee1d33afdb9
Qvei <serg198590@gmail.com>
2022-06-29 14:42:49 +0300

    filter

--- resources/views/filter.blade.php
+++ resources/views/filter.blade.php
@@ -84,7 +84,7 @@
         
         
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <script src="https://weather-g.herokuapp.com/public/scripts.js"></script>
+  <script src="<?php echo public_path(); ?>/scripts.js"></script>
   
     </body>
 </html>
