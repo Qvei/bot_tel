fa8b5c037e1022c2f2c700288627a9de478765ff
Qvei <serg198590@gmail.com>
2022-06-29 15:33:39 +0300

    filter

--- resources/views/filter.blade.php
+++ resources/views/filter.blade.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <title>DB search and xls create</title>
         
-<link rel="stylesheet" href="/public/css/allz.css">
+<link rel="stylesheet" href="/css/allz.css">
   </head>
     <body class="sb-nav-fixed">
       <header>
