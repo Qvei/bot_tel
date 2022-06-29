643cddc964a69e6cd0e8de50f359560965e450f8
Qvei <serg198590@gmail.com>
2022-06-29 18:00:27 +0300

    filter

--- app/Http/Controllers/FilterController.php
+++ app/Http/Controllers/FilterController.php
@@ -19,23 +19,23 @@ class FilterController extends Controller
 	$dt->connect($dbhost=$_SESSION['pars'][0],$dbname=$_SESSION['pars'][1],$username=$_SESSION['pars'][2],$password=$_SESSION['pars'][3]);
 }
 
-if(isset($_POST['localhost']) && $_POST['dbname'] !== ''){
+if(isset($request->localhost) && $request->dbname !== ''){
 	unset($_SESSION['pars']);
-	$dt->pars($_POST['localhost'],$_POST['dbname'],$_POST['username'],$_POST['pas']);
-	$dt->connect($dbhost=$_POST['localhost'],$dbname=$_POST['dbname'],$username=$_POST['username'],$password=$_POST['pas']);
+	$dt->pars($request->localhost,$request->dbname,$request->username,$request->pas);
+	$dt->connect($dbhost=$request->localhost,$dbname=$request->dbname,$username=$request->username,$password=$request->pas);
 	echo $dt->tables();
 }
 
-if(isset($_POST['tabll']) && $_POST['tab']==='tab'){
-	echo $dt->get_cells($base = $_POST['tabll']);
+if(isset($request->tabll) && $request->tab==='tab'){
+	echo $dt->get_cells($base = $request->tabll);
 }
 
-if(isset($_POST['selll']) && isset($_POST['tabll'])){
-	echo $dt->get_c($categ=$_POST['selll'],$base=$_POST['tabll']);
+if(isset($request->selll) && isset($request->tabll)){
+	echo $dt->get_c($categ=$request->selll,$base=$request->tabll);
 }
 
-if(isset($_POST['tabll']) && isset($_POST['vals']) && isset($_POST['ca1'])){
-	echo $dt->get_dbres($cats = $_POST['ca1'],$vals = $_POST['vals'],$table = $_POST['tabll']);
+if(isset($request->tabll) && isset($request->vals) && isset($request->ca1)){
+	echo $dt->get_dbres($cats = $request->ca1,$vals = $request->vals,$table = $request->tabll);
 }
 
 if(isset($_POST['dawadu'])){
