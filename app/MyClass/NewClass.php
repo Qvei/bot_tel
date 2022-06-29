6373cb4e2af002f712233ae32ffa11cdd07a0adb
Qvei <serg198590@gmail.com>
2022-06-29 18:24:32 +0300

    filter

--- app/MyClass/NewClass.php
+++ app/MyClass/NewClass.php
@@ -52,7 +52,7 @@ class NewClass{
 
     public function addWeatherAnswer(){
                 $urll = file_get_contents("https://api.openweathermap.org/data/2.5/onecall?lat=".$this->latitude."&lon=".$this->longitude."&exclude=daily&lang=ua&appid=".$this->api_key);
-                $wear_ans = json_decode($url, true);
+                $wear_ans = json_decode($urll, true);
                     $chas = '';
                     $desc = '';
                     $hourly_today = "<b>Сьогодні</b> \n\n";
